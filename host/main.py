798c40641276798a9643af670f6151220fbf696b
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-08-12 12:47:23 +0200

    Added test data to main.py

--- host/main.py
+++ host/main.py
@@ -1,9 +1,16 @@
 import telemetry.f1_2020 as f1_2020
+import utils.build_packet as build_packet
+import utils.send_packet as send_packet
+
+import utils.data_structure as data_structure # DEBUG
 
 def main():
     try:
-        while True:
-            data = f1_2020.run()
+        data = data_structure.initialize_data()  # DEBUG
+        if data:
+            json_body = build_packet.create_json_packet(data)
+            if json_body:
+                send_packet.send_packet(json_body)
     except KeyboardInterrupt:
         print("\nScript interrupted by user.")
 
